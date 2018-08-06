790080e2cfa1e031f5ff4f36b25768260fa1d724
pjadda <jpavanaryan@gmail.com>
2018-08-06 16:34:14 -0400

    Updated config to MongoDB

--- src/main/java/com/books/repo/BookRepository.java
+++ src/main/java/com/books/repo/BookRepository.java
@@ -1,26 +1,20 @@
 package com.books.repo;
 
 import com.books.model.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 import java.util.Optional;
 
-@Repository
-public interface BookRepository extends JpaRepository<Book,Long>
+
+public interface BookRepository extends MongoRepository<Book,Long>
 {
     List<Book> findAll();
-
-    Book findBookById(long id);
 
     Optional<Book> findById(Long id);
 
     <S extends Book> List<S> saveAll(Iterable<S> var1);
 
     <S extends Book> S saveAndFlush(S var1);
-
-
-    void flush();
 
 }
