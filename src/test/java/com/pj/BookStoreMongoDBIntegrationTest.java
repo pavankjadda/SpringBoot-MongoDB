21bc4583b0f2ccf33b0eb4e4789f4085ebd9dd17
jaddap2 <jaddap2@nih.gov>
2021-02-10 15:23:36 -0500

    Update Test cases to JUnit 5

--- src/test/java/com/pj/BookStoreMongoDBIntegrationTest.java
+++ src/test/java/com/pj/BookStoreMongoDBIntegrationTest.java
@@ -3,14 +3,14 @@ package com.pj;
 import com.pj.model.Book;
 import com.pj.service.BookService;
 import com.pj.util.JsonUtil;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
@@ -22,94 +22,94 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = BookStoreApplication.class)
 @AutoConfigureMockMvc
 @TestPropertySource(locations = "classpath:application-mongodb-integrationtest.properties")
-public class BookStoreMongoDBIntegrationTest
+class BookStoreMongoDBIntegrationTest
 {
-    @Autowired
-    MockMvc mockMvc;
+	@Autowired
+	MockMvc mockMvc;
 
     /*@Autowired
     BookRepository bookRepository;*/
 
-    @Autowired
-    BookService bookService;
+	@Autowired
+	BookService bookService;
 
 
-    @Test
-    public void createPersons_And_GetPersonsTest() throws Exception
-    {
-        Book book=createBook("Spring Boot Essentials",200,12.23,"Craig");
-        mockMvc.perform(get("/books").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andExpect(jsonPath("$[0].title", is(book.getTitle())));
-    }
+	@Test
+	void createPersons_And_GetPersonsTest() throws Exception
+	{
+		Book book = createBook("Spring Boot Essentials", 200, 12.23, "Craig");
+		mockMvc.perform(get("/books").contentType(MediaType.APPLICATION_JSON)).andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)).andExpect(jsonPath("$[0].title", is(book.getTitle())));
+	}
 
-    @Test
-    public void whenValidInput_createPerson() throws Exception
-    {
-        Book book=createBook("Java Edition 9",200,12.23,"Craig");
-        mockMvc.perform(post("/books/save_book").contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJson(book)));
-        List<Book> retrievedBooks = bookService.findAll();
-        for(Book retrievedBook : retrievedBooks)
-        {
-            if(retrievedBook.getTitle().equals("Java Edition 9"))
-                System.out.println("Success");
-        }
-        //assertThat(retrivedBooks).extracting(Book::getTitle).isEqualTo("Spring Essentials");
-    }
+	private Book createBook(String title, Integer numberOfPages, Double cost, String author)
+	{
+		Book book = new Book(title, numberOfPages, cost, author);
+		bookService.insertBook(book);
+		return book;
+	}
 
-    private Book createBook(String title, Integer numberOfPages, Double cost, String author)
-    {
-        Book book=new Book(title,numberOfPages,cost,author);
-        bookService.insertBook(book);
-        return book;
-    }
+	@Test
+	void whenValidInput_createPerson() throws Exception
+	{
+		Book book = createBook("Java Edition 9", 200, 12.23, "Craig");
+		mockMvc.perform(post("/books/save_book").contentType(MediaType.APPLICATION_JSON).content(JsonUtil.toJson(book)));
+		List<Book> retrievedBooks = bookService.findAll();
+		for (Book retrievedBook : retrievedBooks)
+		{
+			if (retrievedBook.getTitle().equals("Java Edition 9"))
+				System.out.println("Success");
+		}
+		//assertThat(retrivedBooks).extracting(Book::getTitle).isEqualTo("Spring Essentials");
+	}
 
-    @Test
-    public void getBook()
-    {
-        System.out.println("Book with id: 5b68baab7ab51f15c19908c6 ==>"+bookService.findById("5b68baab7ab51f15c19908c6"));
-        System.out.println("Book with id: 0 ==> "+bookService.findById("0"));
-    }
+	@Test
+	void getBook()
+	{
+		System.out.println("Book with id: 5b68baab7ab51f15c19908c6 ==>" + bookService.findById("5b68baab7ab51f15c19908c6"));
+		System.out.println("Book with id: 0 ==> " + bookService.findById("0"));
+	}
 
 
-    @Test
-    public void getAllBooks()
-    {
-        List<Book>  books=bookService.findAll();
-        for (Book book : books)
-        {
+	@Test
+	void getAllBooks()
+	{
+		List<Book> books = bookService.findAll();
+		for (Book book : books)
+		{
 
-            System.out.println("Book: " + book);
-        }
+			System.out.println("Book: " + book);
+		}
 
-    }
+	}
 
-    @Test
-    public void deleteBookById()
-    {
-        bookService.deleteById("0");
-    }
+	@Test
+	void deleteBookById()
+	{
+		bookService.deleteById("0");
+	}
 
-    @Test
-    public void deleteAllBooks()
-    {
-        bookService.deleteAll();
-    }
+	@Test
+	void deleteAllBooks()
+	{
+		bookService.deleteAll();
+	}
 
-    @Test
-    public void updateBook()
-    {
-        if(bookService.findById("5b69d82cbdae811dade29e31").isPresent())
-        {
-            Book book=bookService.findById("5b69d82cbdae811dade29e31").get();
-            System.out.println("Before Updating Book ==> "+book.toString());
-            book.setCost(100.99);
-            book.setNumberOfPages(1000);
-            book.setAuthor("PJ");
-            bookService.updateBook(book);
-        }
+	@Test
+	void updateBook()
+	{
+		if (bookService.findById("5b69d82cbdae811dade29e31").isPresent())
+		{
+			Book book = bookService.findById("5b69d82cbdae811dade29e31").get();
+			System.out.println("Before Updating Book ==> " + book.toString());
+			book.setCost(100.99);
+			book.setNumberOfPages(1000);
+			book.setAuthor("PJ");
+			bookService.updateBook(book);
+		}
 
-    }
+	}
 }
